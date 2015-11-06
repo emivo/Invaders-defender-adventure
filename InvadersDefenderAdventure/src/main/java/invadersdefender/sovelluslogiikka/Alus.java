1a77ca0b8be91e90e5ab18f18d19fd2bed99b567
emivo <emil.airta@helsinki.fi>
2015-11-06 10:13:13 +0200

    testejä tehty ja luokkia päivitetty

--- InvadersDefenderAdventure/src/main/java/invadersdefender/sovelluslogiikka/Alus.java
+++ InvadersDefenderAdventure/src/main/java/invadersdefender/sovelluslogiikka/Alus.java
@@ -6,28 +6,40 @@ import java.util.List;
 /**
  * @author emivo
  */
-public class Alus {
+public class Alus implements Liikkuva {
     
     private int koko; // aluksen sivunpituus
     private Pala pala;
+    private Suunta suunta;
 
     // aluksen x koordinaatti on x + aluksenkoko. samoin y
-    // getsijainti antaa listan x,y pareja
+    // getsijainti antaa listan Paloja
 
     public Alus(int alkuX, int alkuY) {
-        koko = 3; //pariton on kiva
+        this.koko = 3; //pariton on kiva
+
+        this.suunta = null;
         
-        
-        pala = new Pala(alkuX, alkuY);
+        this.pala = new Pala(alkuX, alkuY);
     }
-    
-    public void liiku(Suunta suunta) {
-        pala.liiku(suunta);
+
+    public int getKoko() {
+        return koko;
+    }
+
+    public void setSuunta(Suunta suunta) {
+        this.suunta = suunta;
+    }
+
+    public Suunta getSuunta() {
+        return suunta;
     }
     
     public Ammus ammu() {
-        return new Ammus((pala.getX()+koko/2),pala.getY()+1);
+        // oma alus ampuu aina ylos
+        return new Ammus((pala.getX()+koko/2), pala.getY()-1, Suunta.YLOS);
     }
+    
     
     public List<Pala> sijainti() {
         List<Pala> sijainnit = new ArrayList<>();
@@ -39,5 +51,15 @@ public class Alus {
             }
         }
         return sijainnit;
+    }
+
+    @Override
+    public void liiku() {
+        if (this.suunta != null) {
+            this.pala.liiku(suunta);
+            this.suunta = null;
+        }
+        // alus ei liiku ellei se saa uutta suuntaa
+        // siis alus pysyy paikoillaan, ellei sitä liikuta
     }
 }
