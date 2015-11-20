105be7d7aa3fd14349ff0183c882bc04c8cbf7d4
emivo <emil.airta@helsinki.fi>
2015-11-20 21:45:35 +0200

    Javadoc lisätty

--- InvadersDefenderAdventure/src/main/java/invadersdefender/sovelluslogiikka/Vihollisolio.java
+++ InvadersDefenderAdventure/src/main/java/invadersdefender/sovelluslogiikka/Vihollisolio.java
@@ -1,8 +1,10 @@
 package invadersdefender.sovelluslogiikka;
 
 /**
- *
- * @author Emil
+ *  Luokka toimii samoin tavoin kuin yläluokkansa, mutta sisältää {@code Suunta} muuttujan, jonka avulla voidaan määritellä olion liikkumista.
+ * Poiketen {@link Alus} luokan olioihin, tämän luokan oliot {@code ammu()} metodi luo ammukset olion alapuolelle ja niiden suunta on alas.
+ * 
+ * @author emivo
  */
 public class Vihollisolio extends Alus {
 
@@ -18,15 +20,27 @@ public class Vihollisolio extends Alus {
         return new Ammus(getX() + getKoko() / 2, getY() + getKoko() + 1, Suunta.ALAS);
     }
 
+    /**
+     * Palauttaa suunnan, johon olio on seuraavan kerran liikkumassa
+     * @return suunta, johon olio on liikkumassa
+     */
     public Suunta getSuunta() {
         return suunta;
     }
 
+    /**
+     * Asettaa olion liikkumissuunnan, johon seuraavan kerran liikutaan, kun kutsutaan {@code liiku()}
+     * @param suunta Suunta, johon halutaan olion liikkuvan
+     */
     public void setSuunta(Suunta suunta) {
         this.suunta = suunta;
     }
-    
+
+    /**
+     * metodi liikuttaa oliota, sen sille asetetun suunnan mukaan
+     * @see setSuunta(Suunta)
+     */
     public void liiku() {
-        liiku(suunta);
+        super.liiku(suunta);
     }
 }
