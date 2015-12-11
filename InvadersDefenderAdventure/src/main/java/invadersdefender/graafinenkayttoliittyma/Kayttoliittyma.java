bce408e0d64c4206b6a8f48367b504c2e47be2e8
Airta Emil T S <airta@da5-cs-bk107-07.helsinki.fi>
2015-12-11 16:06:13 +0200

    korjaus toimintaan

--- InvadersDefenderAdventure/src/main/java/invadersdefender/graafinenkayttoliittyma/Kayttoliittyma.java
+++ InvadersDefenderAdventure/src/main/java/invadersdefender/graafinenkayttoliittyma/Kayttoliittyma.java
@@ -7,6 +7,7 @@ import javax.swing.WindowConstants;
 
 /**
  * Luokka luo pelille JFrame ikkunan ja lisää Nappaimistokuuntelijan ikkunalle
+ *
  * @author emivo
  */
 public class Kayttoliittyma implements Runnable {
@@ -26,18 +27,18 @@ public class Kayttoliittyma implements Runnable {
     public void run() {
         ikkuna = new JFrame("Invaders defender adventures");
         int reuna = 6;
-        // ikkunaa pitää hieman suurentaa ainakin ubuntulla ja mintillä
+//         ikkunaa pitää hieman suurentaa ainakin ubuntulla ja mintillä
         if (System.getProperty("os.name").startsWith("Linux")) {
-            reuna += 10;
+            reuna = 10;
         }
-        int ikkunanLeveys = peli.getPelikentta().getPelikentanLeveys()* palojenKoko + reuna;
-        int ikkunanKorkeus = peli.getPelikentta().getPelikentanKorkeus() * palojenKoko + 43 + reuna;
+        int ikkunanLeveys = peli.getPelikentta().getPelikentanLeveys() * palojenKoko + reuna;
+        int ikkunanKorkeus = peli.getPelikentta().getPelikentanKorkeus() * palojenKoko + 49;
         piirtoalusta.setIkkuna(ikkuna);
-        
+
         ikkuna.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         rakennaIkkunanKomponentit();
-        ikkuna.setPreferredSize(new Dimension(ikkunanLeveys, ikkunanKorkeus + ikkuna.getJMenuBar().getHeight() ));
+        ikkuna.setPreferredSize(new Dimension(ikkunanLeveys, ikkunanKorkeus + ikkuna.getJMenuBar().getHeight()));
 
         ikkuna.pack();
         ikkuna.setVisible(true);
@@ -50,14 +51,15 @@ public class Kayttoliittyma implements Runnable {
     }
 
     /**
-     * Metodi lisää pelinpiirtoalustan ikkunaan sekä valikon ja näppäimistökuuntelijan
+     * Metodi lisää pelinpiirtoalustan ikkunaan sekä valikon ja
+     * näppäimistökuuntelijan
      */
     public void rakennaIkkunanKomponentit() {
-        
-        ikkuna.getContentPane().add(piirtoalusta);
-        ikkuna.setJMenuBar(new Valikko(peli, ikkuna).luoValikko());
 
+        ikkuna.setJMenuBar(new Valikko(peli, ikkuna).luoValikko());
         ikkuna.addKeyListener(new Nappaimistokuuntelija(peli));
+        ikkuna.getContentPane().add(piirtoalusta);
+
     }
 
 }
